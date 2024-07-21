71550b8f1bcc6fbb50a837c42f220d22420d5e0b
进恩哥资源仓库 <119514614+jinenge@users.noreply.github.com>
2024-07-21 11:47:31 +0800

    Add files via upload

---
+++ js/音乐数据.js
@@ -0,0 +1,122 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    order: 'random',
+    preload: 'auto',
+    listMaxHeight: '336px',
+    volume: '0.5',
+    mutex: true,
+    lrcType: 3,
+    /* 下方更改为你自己的歌单就行 */
+    audio: [{
+     name: "洪荒之力(纯音乐)",
+            artist: "进恩哥资源仓库",
+            url: "./kodbox/data/files/202404/14_cc3cf09d/小瑞 - PDD洪荒之力 (纯音乐).mp3",
+           cover: "https://jinenyy.vip/wallpaper",
+            lrc: "",
+            theme: "#171513"
+        },
+            {
+            name: "寂寞沙洲冷",
+            artist: "周传雄",
+            url: "./kodbox/data/files/202404/14_cc3cf09d/周传雄 - 寂寞沙洲冷.mp3",
+           cover: "https://jinenyy.vip/wallpaper",
+            lrc: "./js/周传雄 – 寂寞沙洲冷.lrc",
+            theme: "#171514"
+        },
+       ]
+});
+
+/* 底栏歌词 */
+setInterval(function () {
+    $("#lrc").html("<span class='lrc-show'><i class='iconfont icon-music'></i> " + $(".aplayer-lrc-current").text() + " <i class='iconfont icon-music'></i></span>");
+}, 500);
+
+/* 音乐通知及控制 */
+ap.on('play', function () {
+    music = $(".aplayer-title").text() + $(".aplayer-author").text();
+    iziToast.info({
+        timeout: 8000,
+        iconUrl: './img/icon/music.png',
+        displayMode: 'replace',
+        message: music
+    });
+    $("#play").html("<i class='iconfont icon-pause'>");
+    $("#music-name").html($(".aplayer-title").text() + $(".aplayer-author").text());
+    if ($(document).width() >= 990) {
+        $('.power').css("cssText", "display:none");
+        $('#lrc').css("cssText", "display:block !important");
+    }
+});
+
+ap.on('pause', function () {
+    $("#play").html("<i class='iconfont icon-play'>");
+    if ($(document).width() >= 990) {
+        $('#lrc').css("cssText", "display:none !important");
+        $('.power').css("cssText", "display:block");
+    }
+});
+
+//音量调节
+function changevolume() {
+    var x = $("#volume").val();
+    ap.volume(x, true);
+    if (x == 0) {
+        $("#volume-ico").html("<i class='iconfont icon-volume-x'></i>");
+    } else if (x > 0 && x <= 0.3) {
+        $("#volume-ico").html("<i class='iconfont icon-volume'></i>");
+    } else if (x > 0.3 && x <= 0.6) {
+        $("#volume-ico").html("<i class='iconfont icon-volume-1'></i>");
+    } else {
+        $("#volume-ico").html("<i class='iconfont icon-volume-2'></i>");
+    }
+}
+
+$("#music").hover(function () {
+    $('.music-text').css("display", "none");
+    $('.music-volume').css("display", "flex");
+}, function () {
+    $('.music-text').css("display", "block");
+    $('.music-volume').css("display", "none");
+})
+
+/* 一言与音乐切换 */
+$('#open-music').on('click', function () {
+    $('#hitokoto').css("display", "none");
+    $('#music').css("display", "flex");
+});
+
+$("#hitokoto").hover(function () {
+    $('#open-music').css("display", "flex");
+}, function () {
+    $('#open-music').css("display", "none");
+})
+
+$('#music-close').on('click', function () {
+    $('#music').css("display", "none");
+    $('#hitokoto').css("display", "flex");
+});
+
+/* 上下曲 */
+$('#play').on('click', function () {
+    ap.toggle();
+    $("#music-name").html($(".aplayer-title").text() + $(".aplayer-author").text());
+});
+
+$('#last').on('click', function () {
+    ap.skipBack();
+    $("#music-name").html($(".aplayer-title").text() + $(".aplayer-author").text());
+});
+
+$('#next').on('click', function () {
+    ap.skipForward();
+    $("#music-name").html($(".aplayer-title").text() + $(".aplayer-author").text());
+});
+
+/* 打开音乐列表 */
+$('#music-open').on('click', function () {
+    if ($(document).width() >= 990) {
+        $('#box').css("display", "block");
+        $('#row').css("display", "none");
+        $('#more').css("cssText", "display:none !important");
+    }
+});
